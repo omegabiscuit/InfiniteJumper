9e3d506c957a62d3179ed8af61fe205596db1002
Brigham Bangerter <bb3uc@virginia.edu>
2017-11-04 23:45:18 -0500

    first commit

---
+++ Assets/Standard Assets/2D/Scripts/BouncerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncerScript : MonoBehaviour {
+
+    public float bounceFactor = 4f;
+
+    public GameObject spawner;
+    bool hasSpawned = false;
+
+	// Use this for initialization
+	void Start () {
+        spawner = GameObject.FindGameObjectWithTag("Respawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
+
+        if (!hasSpawned)
+        {
+            spawner.SendMessage("Spawn");
+            hasSpawned = true;
+        }
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/CameraFollow1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow1 : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(player.transform.position.y > 0)
+        {
+            if(player.transform.position.y > transform.position.y)
+            {
+                this.transform.position = new Vector3(5, player.transform.position.y, -10);
+            }
+        }
+	}
+}
